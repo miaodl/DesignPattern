63355f90a0a20eee4efa40bffb160f3be1c226cd
miaodl <421521434@qq.com>
2015-11-07 05:51:21 +0800

    add VisitorPattern

---
+++ 21100VisitorPattern/Program.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21100VisitorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    //第二十五章 访问者模式
+    //Represent an operation to be performed on the elements of an object structure.Visitor lets you define a new operation without changing the classes of the elements on which it operates
+
+    /*
+     * 由于应用开发过程中先前完成的类型会因为需求变化（无论是业务功能，还是技术实现火石处于集成的需要）增加新的方法，如果直接在基类中增加新的方法，其派生类可能需要相应的比较繁琐的处理。因此，如何在不改变既有类型层次的前提下，运行时动态为类型的每个类增加新的操作就成了访问者模式视图解决的问题。
+     * 
+     * Page 424 461
+     */
+
+    //25.2经典回顾
+    /*
+     *  访问者模式的使用情况：
+     *  
+     * 1，一个类型需要依赖很多不同接口的类型，在结构尽量松散的前提下，希望可以用到这些类型不同的接口方法
+     * 
+     * 2，经常需要为一个结构相对固定的对象结构添加一些新的操作
+     * 
+     * 3，需要用一个独立的类型来组织一批不相干的操作，使用他的类型可以根据应用需要进行定制 
+     * 
+     *                                                          
+     *                            ____________Client_______                     
+     *                           /                         \     
+     *                          /                           \   
+     *                         /                             \    
+     *                        /                               \   
+     *                       /                                 \  
+     *   objectStrure<------     interface                       interface              
+     *         |---------------->IElement                        IVisitor                         
+     *                          +Accept(IVistor):void            +VisitElementA():void             
+     *                           /\          /\                  +VisitElementB():void
+     *                          /              \                         /|\
+     *                         /                \                         |
+     *                        /                  \                        |
+     *          ConcreteElementA             ConcreteElementB       ConcreteVisitor           
+     *          
+     * 
+     * 
+     * //IVisitor：声明以一个或多个需要添加的操作
+     * ConcreteVisitor：增加具体操作的类型
+     * IElement:接受IVisitor并使用其操作的抽象类型
+     * ObjectStructure:一个高层接口，用来做组织IElement，一般需要提供遍历每个IElement的能力
+     */
+
+    //visitor 需要影响到的Element,Visitable
+    public interface IEmployee
+    {
+        //相关属性
+        string Name
+        {
+            get;
+            set;
+        }
+        double Income
+        {
+            get;
+            set;
+        }
+        int VacationDays
+        {
+            get;
+            set;
+        }
+
+        //接受IVisitor的方法
+        void Accept(IVisitor visitor);
+    }
+
+    //抽象Visitor接口
+    public interface IVisitor
+    {
+        //以下方法为访问Visiable类提供了具体操作，如何修改IVisiable类在方法中进行
+        void VisitEmployee(IEmployee employee);//这里修改就只限定于IEmployee的具体成员
+        void VisitManager(Manager manager);//这里的修改限定于Manager的具体成员
+    }
+
+    //一个具体Element
+    public class Employee:IEmployee
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private double income;
+
+        public double Income
+        {
+            get { return income; }
+            set { income = value; }
+        }
+        private int vacationDays;
+
+        public int VacationDays
+        {
+            get { return vacationDays; }
+            set { vacationDays = value; }
+        }
+
+        public Employee(string name,double income,int vacationdays)
+        {
+            this.name = name;
+            this.income = income;
+            this.vacationDays = vacationdays;
+        }
+
+        //引入Visitor对自身的操作
+        public virtual void Accept(IVisitor visitor)
+        {
+            visitor.VisitEmployee(this);
+        }
+
+
+    }
+
+    //另一具体的Element
+    public class Manager:Employee//层次的Object Structure
+    {
+        private string department;
+
+        public string Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+        public Manager(string name,int income,int vacationdays,string department):base(name,income,vacationdays)
+        {
+            this.department = department;
+        }
+
+        //引入Visitor对自身的操作
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.VisitManager(this);
+        }
+
+    }
+
+    //为了便于对HR系统的对象进行批量处理增加的集合类型
+    public class EmployeeCollection:List<IEmployee>
+    {
+        //组合起来的批量Accept操作,其中IVisitor为对象结构提供了统一的访问
+        public virtual void Accept(IVisitor visitor)
+        {
+            foreach (IEmployee employee in this)
+                employee.Accept(visitor);
+        }
+    }
+
+    /*
+     * 可尝试在Visitable类中增加一个委托列表或字典，用具体的Visitor来维护类中的委托列表，使类结构增加新的操作方法
+     * 
+     */
+
+    //具体的Visitor类，增加休假天数
+    class ExtraVacationVisitor : IVisitor
+    {
+        public void VisitorEmployee(IEmployee employee)
+        {
+            employee.VacationDays += 1;
+        }
+        public void VisitorManager(Manager manager)
+        {
+            manager.VacationDays += 2;
+        }
+    }
+
+    //具体Visitor，加薪
+    class RaiseSalaryVistor:IVisitor
+    {
+        public void VisitEmployee(IEmployee employee)
+        {
+            employee.Income *= 1.1;
+        }
+        public void VisitManager(Manager manager)
+        {
+            manager.Income *= 1.2;
+        }
+    }
+
+
+    public class Client
+    {
+        public void TestMethod()
+        {
+            EmployeeCollection employees =new EmployeeCollection();
+            employees.Add(new Employee("joe", 2500, 14));
+            employees.Add(new Employee("frank", 4500, 24));
+            employees.Add(new Employee("john", 3500, 22));
+            employees.Add(new Manager("dick", 6500, 34,"Sales"));
+
+            employees.Accept(new ExtraVacationVisitor());
+            employees.Accept(new RaiseSalaryVistor());
+        }
+    }
+
+    /*
+     * 1，Employee类型并没有加薪和修改休假天数的方法，但从效果上看确实获得了相应的功能
+     * 
+     * 2，新增功能通过外部具体IVisitor类型完成的，他们虽然完全独立，但只要被Employee或它的某个子类型请进去就可以完成新的操作
+     * 
+     * 3，访问者模式将有关的行为集中到一个统一抽象的IVisitor上面，而不是每个具体操作的类型上。
+     * 
+     * 但是从静态结构图和示例中我们也会发现访问者模式的一些缺点：
+     *1， 当需要增加新的ConcreteElement的时候，IVisitor需要修改
+     *2，从面向对象的角度看，Employee的封装性被破坏了，因为新增的功能是在Employee外部完成的
+     * 
+     * 
+     */
+
+
+
+}
---
+++ 21101VisitorPatternReflectionImplement/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21101VisitorPatternReflectionImplement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    //25.3借助反射实现Visitor
+
+    /*
+     * 前面的例子虽然实现了经典的访问者模式，不过还是有些别扭，因为Employee和Manager类型都需要知道自己应该调用VisitEmployee和VisitManager方法。这样一方面IVisitor的这两个方法被外部类型显示依赖，另一方面也翻盖IVisitor通过其他模式（策略模式，状态模式，解释器模式）动态加载合适各个IEmployee类型曹组的能力。
+     * 
+     * 下面采用反射实现：
+     * 
+     */
+
+    //visitor 需要影响到的Element,Visitable
+    public interface IEmployee
+    {
+        //相关属性
+        string Name
+        {
+            get;
+            set;
+        }
+        double Income
+        {
+            get;
+            set;
+        }
+        int VacationDays
+        {
+            get;
+            set;
+        }
+
+        //接受IVisitor的方法
+        void Accept(IVisitor visitor);
+    }
+
+    //抽象Visitor接口
+    public interface IVisitor
+    {
+        void Visit(IEmployee employee);
+    }
+
+    //一个具体Element
+    public class Employee : IEmployee
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private double income;
+
+        public double Income
+        {
+            get { return income; }
+            set { income = value; }
+        }
+        private int vacationDays;
+
+        public int VacationDays
+        {
+            get { return vacationDays; }
+            set { vacationDays = value; }
+        }
+
+        public Employee(string name, double income, int vacationdays)
+        {
+            this.name = name;
+            this.income = income;
+            this.vacationDays = vacationdays;
+        }
+
+        //引入Visitor对自身的操作
+        public virtual void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+
+    }
+
+    //另一具体的Element
+    public class Manager : Employee//层次的Object Structure
+    {
+        private string department;
+
+        public string Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+        public Manager(string name, int income, int vacationdays, string department)
+            : base(name, income, vacationdays)
+        {
+            this.department = department;
+        }
+
+        //引入Visitor对自身的操作，所有的子类都依赖于IVisitor，这里的Accept方法可以不用重写了
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+    }
+
+
+    //*******基于反射的Visitor*******
+
+    public class ReflectionVisitor:IVisitor
+    {
+        //通过反射和预定好饿方法命令规则动态执行
+        public void Visit(IEmployee employee)
+        {
+            string typeName = employee.GetType().Name;//获取employee的类型名
+            string methodName = "Visit" + typeName;//使用Visit+类型名拼接关联的方法名
+            MethodInfo method = this.GetType().GetMethod(methodName);//获取反射Visitor类的关联employee的方法
+            method.Invoke(this, new object[] { employee });//调用这个关联的方法，并把employee作为参数传入，这里实现了一个动态的调用
+        }
+
+        //这样对相关类的所有Visit访问操作，都写在这两个方法里,并且只依赖于IEmployee接口
+        public void VisitEmployee(IEmployee employee)
+        {
+            employee.Income *= 1.1;
+            employee.VacationDays += 1;
+        }
+        public void VisitManager(IEmployee employee)
+        {
+            Manager manager = (Manager)employee;
+            manager.Income *= 1.2;
+            manager.VacationDays += 2;
+        }
+
+    }
+    //**********************
+    //这里还可以经一部调整把具体类型与应该完成Visit的方法卸载配置文件里，这样可以大大减少经典访问者模式
+    /*
+     * <configSetions>
+     *      <section name="marvellousWorks.practicalPattern.visitorPatten" type="System.Configuration.NameValueSectionHandler"/>
+     * </configSections> 
+     * <marvellousWorks.practicalPattern.visitorPatten>
+     *      <add key="Employee" value="VisitCommonEmployee"/>
+     *      <add key="Manager" value="VisitDepartmentManager"/>
+     * </marvellousWorks.practicalPattern.visitorPatten>
+     * 
+     * 
+     * System.Configuration.NameValueSectionHandler是系统默认的Section操作类
+     */
+
+    //Page 430 467
+
+
+    //***************************************
+    //25.4用委托是引用关系更加松散
+    //用委托把上面IVisitor中对IEmployee的匹配过程抽象出来，，，或者采用委托或事件机制注入具体的访问操作
+
+
+
+    //***************************************
+    //小结
+    /*
+     * 访问者模式是最复杂的一个模式，访问者模式被限制在一个相对特定的类型体系环境下，实现上非常有技巧性，项目实施中我们应该考虑这种复杂性是否有必要：
+     * 
+     * 1，双因素依赖倒置调试困难
+     * 2，相对定位更加困难。
+     * 3，打破面向对象的封装性。
+     * 4，执行效率问题。
+     * 5，通过反射+配置文件的方式可以解决因为类型结构扩大带来访问者的脆弱性。
+     * 
+     * 项目中是否使用访问者模式，还需要多权衡。
+     * 
+     * 
+     */
+}
